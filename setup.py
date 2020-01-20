c43bfed0c4b935c23b5cf87d99c9118b6de888b2
lewisgmorris <Lewis.morris@gmail.com>
2020-01-20 05:07:44 +0000

    hyperopt update

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from setuptools import setup
 
 setup(
     name='Skperopt',
-    version='0.0.1',
+    version='0.0.2',
     packages=["skperopt"],
     url='https://github.com/lewis-morris/Skperopt',
     license='MIT',
     author='Lewis',
     author_email='lewis.morris@gmail.com',
     description='Hyperopt Wrapper',
-    instal_requires=requirements,
+    install_requires=requirements,
     download_url = 'https://github.com/lewis-morris/Skperopt/archive/0.0.1.tar.gz',
     keywords=['hyperopt-wrapper', 'hyperparameter'],
     classifiers=[
